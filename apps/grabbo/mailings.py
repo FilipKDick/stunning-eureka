11a54e8dcc6bf78cb5c6a2330eb87a13b4bacfca
Jan Zysko <janzysko@gmail.com>
2025-02-24 17:36:36 +0100

    remove header

--- apps/grabbo/mailings.py
+++ apps/grabbo/mailings.py
@@ -2,7 +2,7 @@ from django.core.mail import EmailMessage
 
 
 def send_mail_with_offers(offers: list):
-    content = '<h1>SIEMA LENA</h1><br><br><h2>Nowe oferty:</h2><br>'
+    content = '<br><h2>Nowe oferty:</h2><br>'
     for offer in offers:
         company = offer.get('company__name', offer.get('company'))
         content += f'<a href="{offer["url"]}">{offer["title"]} w {company}</a><br>'
